3e24c400fcfb3f9b457c201183fa9a2c25450d40
romancode <ar_roman@yahoo.com>
2014-09-16 10:56:29 +0600

    Initial commit

---
+++ src/com/example/searchlistviewdemo/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.searchlistviewdemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	ListView lv;
+	ArrayAdapter<String> adapter;
+
+	String[] products;
+
+	EditText inputSearch;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Listview Data
+		String products[] = {"Dell Inspiron", "HTC One X", "HTC Wildfire S", "HTC Sense", "HTC Sensation XE",
+				"iPhone 4S", "Samsung Galaxy Note 800",
+				"Samsung Galaxy S3", "MacBook Air", "Mac Mini", "MacBook Pro"};
+
+		lv=(ListView)findViewById(R.id.list_view);
+		inputSearch=(EditText)findViewById(R.id.inputSearch);
+
+		adapter= new ArrayAdapter<String>(this, R.layout.list_item,R.id.product_name, products);
+		lv.setAdapter(adapter);
+
+
+
+		/**
+		 * Enabling Search Filter
+		 * */
+		inputSearch.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+				// When user changed the Text
+				MainActivity.this.adapter.getFilter().filter(cs);  
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				// TODO Auto-generated method stub                         
+			}
+		});
+	}
+
+}
